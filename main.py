8269551c97d79827ddb36cbeb6839f52b47c4903
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-30 20:38:57 +0700

    Add print missing states to CSV file when press 'cancel'

--- main.py
+++ main.py
@@ -20,20 +20,27 @@ sc.addshape(image)
 turtle.shape(image)
 
 states = []
-# read data from csv
+# read data from CSV
 data = pandas.read_csv("50_states.csv")
 name_states = data["state"].to_list()
 
 game_is_on = True
 num_guessed_states = 0
 while game_is_on and num_guessed_states != 50:
-    answer = sc.textinput(title="Guess the State", prompt=f"What's another state's name?[{num_guessed_states}/50]").title()
+    answer = sc.textinput(title="Guess the State", prompt=f"What's another state's name?[{num_guessed_states}/50]")
     for state in name_states:
-        if answer == state:
-            t = State(state, int(data[data.state == state]["x"]), int(data[data.state == state]["y"]))
+        if answer is None:
+            game_is_on = False
+        elif answer.title() == state:
+            x = int(data[data.state == state]["x"])
+            y = int(data[data.state == state]["y"])
+            t = State(state, x, y)
             name_states.remove(state)
             num_guessed_states += 1
-        elif answer is None:
-            game_is_on = False
+
+# print missing states to CSV
+if num_guessed_states != 50:
+    missing_states = pandas.DataFrame(name_states)
+    missing_states.to_csv("missing_states.csv")
 
 sc.exitonclick()
