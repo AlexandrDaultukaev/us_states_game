78fc17eec737be33eeb9746b96743c16548b8b13
Alexandr Daultukaev <shyra31102000@mail.ru>
2021-07-30 19:30:21 +0700

    Make reading from CSV file

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ class State(turtle.Turtle):
         self.penup()
         self.hideturtle()
 
-    def set_name(self, name, x, y):
+    def set_name(self, name_s, x, y):
         self.goto(x, y)
-        self.name = name
-        self.write(name, align="center", font=("Arial", 20, "normal"))
+        self.name = name_s
+        #self.write(name, align="center", font=("Arial", 20, "normal"))
+
 
 
 sc = turtle.Screen()
@@ -23,7 +24,17 @@ sc.addshape(image)
 turtle.shape(image)
 
 answer = sc.textinput(title="Guess the State", prompt="What's another state's name?")
+states = []
 
+# read data from csv
 data = pandas.read_csv("50_states.csv")
+name_states = data["state"]
+for name in name_states:
+    # Example: name = Alamaba, data_one_state = state   x   y
+    #                                           Alabama 139 -77
+    data_one_state = data[data["state"] == name]
+    state = State()
+    state.set_name(name, int(data_one_state["x"]), int(data_one_state["y"]))
+    states.append(state)
 
 sc.exitonclick()
